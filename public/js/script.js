9add22c0b50f98811c876efd0e6947287e8b2dfd
Alejandro Roa <alejodev04@gmail.com>
2020-05-05 08:57:10 -0400

    validate functions

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,41 @@
-console.log("ready")
+let button = document.getElementById("action")
+
+button.addEventListener("click", function () {
+    getData()
+ })
+ 
+
+function getData(){
+    let validateName = document.getElementById("validate-name")
+    let validateLastName = document.getElementById("validate-lastName")
+    let name = document.getElementById("name").value
+    let lastName = document.getElementById("lastName").value
+
+    validate(name, validateName, lastName, validateLastName)
+}
+
+function validate(name, validateName, lastName, validateLastName){
+
+    if (name) {
+        validateName.classList.remove("validate-text-active")
+        validateName.className = ("validate-text-inactive")
+    }else{
+        validateName.classList.remove("validate-text-inactive")
+        validateName.className = ("validate-text-active")
+    }
+    if (lastName) {
+        validateLastName.classList.remove("validate-text-active")
+        validateLastName.className = ("validate-text-inactive")
+    }else{
+        validateLastName.classList.remove("validate-text-inactive")
+        validateLastName.className = ("validate-text-active")
+    }
+
+    if(name && lastName){
+        reserve()
+    }
+}
+
+function reserve(){
+    console.log("reservando..")
+}
