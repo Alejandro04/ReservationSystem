6170682c12d5b34cf99f24e663f56bd87aed579e
Alejandro Roa <alejodev04@gmail.com>
2020-05-05 19:28:57 -0400

    test

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(require('./routes/index'));
 app.use(require('./routes/admin'));
 
 // Static Files
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static('public'));
 
 // Server is listenning
 app.listen(app.get('port'), () => {
