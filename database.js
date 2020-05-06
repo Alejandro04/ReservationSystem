db494842d12f45a61f8e82c46f513a202d8df29a
Alejandro Roa <alejodev04@gmail.com>
2020-05-06 10:24:49 -0400

    delete item

--- database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://localhost/notes-db', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useFindAndModify: false
-})
-    .then(db => console.log('db connect'))
-    .catch(err => console.log(err))
